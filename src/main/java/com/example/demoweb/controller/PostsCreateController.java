ed7fa9ce3741758eaa00e9a9aa0987d7fa3393ba
sphinxan <dazaj.osamu@mail.ru>
2022-09-19 01:11:57 +0500

    7. Создание нового поста

---
+++ src/main/java/com/example/demoweb/controller/PostsCreateController.java
@@ -0,0 +1,31 @@
+package com.example.demoweb.controller;
+
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostsCreateController {
+
+    @Autowired
+    PostService postsService;
+
+    @ResponseBody
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create(Model model) {
+        return "create";
+    }
+
+    // Метод обработки запроса
+    // метод который обрабатывает http запросы на тот же url но методом POST.
+    // И в качестве аргумента принимал бы @ModelAttribute("text") String text
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text){
+        postsService.create(text);
+        //Сделайте редирект в методе doCreate, для этого возвращайте
+        return "redirect:/";
+    }
+
+}
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -10,11 +11,18 @@ import java.util.List;
 @Service
 public class PostService {
 
+    List<Post> posts = new ArrayList<>();
+
     //метод возвращающий список из трёх постов с текстами на ваше усмотрение
     public List<Post> listAllPosts(){
-        List<Post> posts = Arrays.asList(new Post("first", 1, new Date(2020,01,01)),
-                new Post("second", 2, new Date(2021,01,01)),
-                new Post("third", 3, new Date(2022,01,01)));
         return posts;
+    }
+
+    //Сервис для добавления поста
+    public void create(String text) {
+        //posts.add(new Post("first", 1, new Date(2020, 01, 01)));
+        //posts.add(new Post("second", 2, new Date(2021, 01, 01)));
+        //posts.add(new Post("third", 3, new Date(2022, 01, 01)));
+        posts.add(new Post(text, 0, new Date()));
     }
 }
